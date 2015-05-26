f6a5f4112ce4a2df6db02c1006e90fbda3f9b7fa
Alexandre de Lima <allima1991@gmail.com>
2015-05-26 12:17:27 -0300

    teste

--- Futebol/src/br/com/k19/filters/ControleDeAcesso.java
+++ Futebol/src/br/com/k19/filters/ControleDeAcesso.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter(servletNames = { "Faces Servlet" })
+//@WebFilter(servletNames = { "Faces Servlet" })
 public class ControleDeAcesso implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
